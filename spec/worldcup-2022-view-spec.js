fb7b4b0e6adffc8c7bc3150035d237d70f1524c4
SBOAAAA <fun_pu@hotmail.com>
2022-11-11 14:25:41 +0700

    edit

---
+++ spec/worldcup-2022-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Worldcup2022View from '../lib/worldcup-2022-view';
+
+describe('Worldcup2022View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
